76f7f638aaf9e53f9973a540b39713e72cd22736
eung-eung <boyvip5231@gmail.com>
2026-04-22 16:28:45 +0700

    feat(auth): add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate, Outlet } from 'react-router-dom'
 
 import Board from '~/pages/Boards/_id'
 import NotFound from './pages/404/NotFound'
 import Auth from './pages/Auth/Auth'
+import AccountVerification from './pages/Auth/AccountVerification'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from './redux/user/userSlice'
 
+const ProtectedRoute = ({ user }) => {
+  if (!user) return <Navigate to='/login' replace={true} />
+  return <Outlet />
+}
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
   return (
     <Routes>
       {/* Redirect route */}
       <Route path='/' element={
         <Navigate to="/boards/69d5fa000905b348bbce8505" replace={true} />
       } />
-
-      {/* board detail */}
-      <Route path="/boards/:boardId" element={<Board />} />
-
+      <Route element={<ProtectedRoute user={currentUser}/>}>
+        {/* board detail */}
+        <Route path="/boards/:boardId" element={<Board />} />
+      </Route>
       {/* Authentication */}
       <Route path='/login' element={ <Auth /> }/>
       <Route path='/register' element={ <Auth /> } />
+      <Route path='/account/verification' element={ <AccountVerification /> } />
 
       {/* 404 not found page */}
       <Route path='*' element={<NotFound />} />
