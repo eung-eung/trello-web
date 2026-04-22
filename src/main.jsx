5a3dd44d6a11017310dd646e6a55d412ecee8241
eung-eung <boyvip5231@gmail.com>
2026-04-22 16:29:36 +0700

    feat(auth): persist user state with redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from '~/theme.js'
@@ -9,18 +9,23 @@ import CustomToastifyContainer from './components/CustomToastifyContainer/Custom
 import { Provider } from 'react-redux'
 import { store } from '~/redux/store.js'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+const persistor = persistStore(store)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename='/'>
       <Provider store={store}>
-        <CssVarsProvider theme={theme}>
-          <CssBaseline />
-          <InteractionLockProvider>
-            <App />
-          </InteractionLockProvider>
-          <CustomToastifyContainer />
-        </CssVarsProvider>
+        <PersistGate persistor={persistor} loading={null}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <InteractionLockProvider>
+              <App />
+            </InteractionLockProvider>
+            <CustomToastifyContainer />
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>
